226d734e9ed68d1a7c3b0fdb81f0e2960a70d802
Nguyen Huu Phuoc <nguyenhuuphuocit97@gmail.com>
2022-02-17 00:09:20 +0700

    :tada: feat: init project #1

---
+++ src/main/java/com/phuocnguyen/app/ngxblobswss/config/NgxWebSocketConfig.java
@@ -0,0 +1,53 @@
+package com.phuocnguyen.app.ngxblobswss.config;
+
+import com.ngxsivaos.model.properties.TunnelSocketProperties;
+import com.phuocnguyen.app.ngxblobssrv.service.NgxTunnelSocketBaseService;
+import com.phuocnguyen.app.ngxblobssrv.service.NgxWebsocketBaseService;
+import com.phuocnguyen.app.ngxblobssrv.service.serviceImpl.NgxTunnelSocketBaseServiceImpl;
+import com.phuocnguyen.app.ngxblobssrv.service.serviceImpl.NgxWebsocketBaseServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
+@SuppressWarnings({"All"})
+@Configuration
+@EnableWebSocket
+public class NgxWebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private TunnelSocketProperties tunnelSocketProperties;
+
+    @Autowired
+    private NgxTunnelSocketBaseService ngxTunnelSocketBaseService;
+
+    @Bean
+    public TunnelSocketProperties tunnelSocketProperties() {
+        return new TunnelSocketProperties();
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxBinaryMessageBufferSize(1024000);
+        return container;
+    }
+
+    @Bean
+    public NgxWebsocketBaseService ngxWebsocketBaseService() {
+        return new NgxWebsocketBaseServiceImpl();
+    }
+
+    @Bean
+    public NgxTunnelSocketBaseService ngxTunnelSocketBaseService() {
+        return new NgxTunnelSocketBaseServiceImpl();
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        ngxTunnelSocketBaseService.registerTunnelsSocketHandlers(registry, ngxWebsocketBaseService(), tunnelSocketProperties);
+    }
+}
---
+++ src/test/java/com/phuocnguyen/app/ngxblobswss/NgxBlobsWssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phuocnguyen.app.ngxblobswss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgxBlobsWssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
